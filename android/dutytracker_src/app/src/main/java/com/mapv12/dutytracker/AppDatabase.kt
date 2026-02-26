6ebf9079d2cc591850538db7772682306797e0b0
YakauKuranets <124173268+YakauKuranets@users.noreply.github.com>
2026-02-26 10:23:03 +0300

    Update AppDatabase.kt

--- android/dutytracker_src/app/src/main/java/com/mapv12/dutytracker/AppDatabase.kt
+++ android/dutytracker_src/app/src/main/java/com/mapv12/dutytracker/AppDatabase.kt
@@ -1,11 +1,18 @@
+package com.mapv12.dutytracker
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.mapv12.dutytracker.scanner.CameraDao
+import com.mapv12.dutytracker.scanner.CameraEntity
+
 @Database(
     entities = [
         TrackPointEntity::class,
         EventJournalEntity::class,
         ChatMessageEntity::class,
-        CameraEntity::class   // добавлено
+        CameraEntity::class
     ],
-    version = 6,   // увеличиваем версию
+    version = 6,               // увеличь версию с 5 до 6
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
